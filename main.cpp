dee217c8312d747b9d79d678d5a0f24a6e79c5ab
Blake Hegerle <blake.hegerle@allianzlife.com>
2024-06-10 09:28:06 -0500

    set name to compy

--- main.cpp
+++ main.cpp
@@ -1,26 +1,32 @@
 #include "platform.h"
 
- #include <llvm/ADT/APFloat.h>
- #include <llvm/ADT/STLExtras.h>
- #include <llvm/IR/BasicBlock.h>
- #include <llvm/IR/Constants.h>
- #include <llvm/IR/DerivedTypes.h>
- #include <llvm/IR/Function.h>
- #include <llvm/IR/IRBuilder.h>
- #include <llvm/IR/LLVMContext.h>
- #include <llvm/IR/Module.h>
- #include <llvm/IR/Type.h>
- #include <llvm/IR/Verifier.h>
+#include <llvm/ADT/APFloat.h>
+#include <llvm/ADT/STLExtras.h>
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Verifier.h>
 
 #include <format>
 #include <iostream>
 #include <string>
 
 using namespace std;
+using llvm::LLVMContext;
 
 int main() {
     try {
         platform_init();
+        
+        auto ctx = LLVMContext();
+
+        cout << format("hi {}", "there") << endl;
+
         return 0;
     } catch (exception &x) {
         cout << "unhandled exception: " << x.what() << endl;
